375af7e6226b876c51111b3ccbf47371ccd0e79d
e-carlin <evan@carlin.com>
2019-08-19 20:11:57 -0600

    Create tables.

--- Backend/src/main/java/idealab/IdeaLabMain.java
+++ Backend/src/main/java/idealab/IdeaLabMain.java
@@ -3,12 +3,14 @@
  */
 package idealab;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IdeaLabMain {
-    
+
+    @Autowired
     public static void main(String[] args) {
         SpringApplication.run(IdeaLabMain.class, args);
     }
--- Backend/src/main/java/idealab/api/controller/PrintJobController.java
+++ Backend/src/main/java/idealab/api/controller/PrintJobController.java
@@ -3,16 +3,19 @@ package idealab.api.controller;
 import idealab.api.dto.GenericResponse;
 import idealab.api.dto.PrintJobUpdateRequest;
 import idealab.api.operations.PrintJobOperations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class PrintJobController {
 
     private final PrintJobOperations printJobOperations;
+
 
     public PrintJobController(PrintJobOperations printJobOperations) {
         this.printJobOperations = printJobOperations;
