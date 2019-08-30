1b24c29e5f691ef55d076cba7a07f8b74cb8e840
will-estes <will_estes@yahoo.com>
2019-08-29 18:14:36 -0600

    12 hour token exp.

--- Backend/src/main/java/idealab/api/security/SecurityConstants.java
+++ Backend/src/main/java/idealab/api/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package idealab.api.security;
 
 public class SecurityConstants {
     public static final String SECRET = "ideaLabSecretKey";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final long EXPIRATION_TIME = 43_200; // 12 Hours
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users/sign-up";
