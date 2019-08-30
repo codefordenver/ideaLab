4e8d80080cdfe9fa28ebe93d593e975822268951
will-estes <will_estes@yahoo.com>
2019-08-29 20:42:41 -0600

    no authenticate needed for non-delete requests

--- Backend/src/main/java/idealab/api/security/WebSecurity.java
+++ Backend/src/main/java/idealab/api/security/WebSecurity.java
@@ -15,9 +15,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import static idealab.api.security.SecurityConstants.LOGIN_URL;
-import static idealab.api.security.SecurityConstants.SIGN_UP_URL;
-
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -34,9 +31,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(SIGN_UP_URL, LOGIN_URL).permitAll()
+                //.antMatchers(SIGN_UP_URL, LOGIN_URL).permitAll()
                 .antMatchers(HttpMethod.DELETE).hasRole("Admin")
-                .anyRequest().authenticated()
+                //.anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager(), employeeRepo))
