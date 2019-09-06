fe652ee07884448375aebf0f957abfa36460be82
will-estes <will_estes@yahoo.com>
2019-09-05 20:03:30 -0600

    UserControllerTest update

--- Backend/src/test/java/idealab/api/controller/UserControllerTest.java
+++ Backend/src/test/java/idealab/api/controller/UserControllerTest.java
@@ -1,72 +1,158 @@
-//package idealab.api.controller;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import idealab.api.dto.GenericResponse;
-//import idealab.api.model.Employee;
-//import idealab.api.operations.UserOperations;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//
-//import static idealab.api.util.TestUtil.stringToGenericResponse;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//@EnableAutoConfiguration
-//public class UserControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Mock
-//    private UserOperations operations;
-//
-//    @InjectMocks
-//    private UserController controller;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        mockMvc = MockMvcBuilders
-//                .standaloneSetup(controller).build();
-//    }
-//
-//    @Test
-//    public void userSignUp() {
-//        GenericResponse genericResponse = new GenericResponse();
-//        genericResponse.setSuccess(true);
-//        genericResponse.setMessage("Print Job Updated");
-//
-//        Employee e = new Employee();
-//
-//        String inputJson = employeeAsJsonString()
-//
-//        String returnJson = mockMvc.perform(put("/api/printjobs/3/status")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(inputJson)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().is2xxSuccessful())
-//                .andReturn().getResponse().getContentAsString();
-//
-//        GenericResponse returnedResponse = stringToGenericResponse(returnJson);
-//        assert (returnedResponse.equals(genericResponse));
-//    }
-//
-//    public String employeeAsJsonString(Object obj) {
-//        try {
-//            final ObjectMapper mapper = new ObjectMapper();
-//            return mapper.writeValueAsString(obj);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//}
+package idealab.api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import idealab.api.dto.response.GenericResponse;
+import idealab.api.model.Employee;
+import idealab.api.operations.UserOperations;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static idealab.api.util.TestUtil.stringToGenericResponse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+@EnableAutoConfiguration
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private UserOperations operations;
+
+    @InjectMocks
+    private UserController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void userSignUp() throws Exception {
+        GenericResponse genericResponse = new GenericResponse();
+        genericResponse.setSuccess(true);
+        genericResponse.setMessage("User Sign Up Successful");
+
+        Employee e = new Employee();
+        e.setUsername("test");
+        e.setPassword("password");
+
+        String inputJson = employeeAsJsonString(e);
+
+        when(operations.userSignUp(any())).thenReturn(genericResponse);
+
+        String returnJson = mockMvc.perform(post("/users/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(inputJson)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn().getResponse().getContentAsString();
+
+        GenericResponse returnedResponse = stringToGenericResponse(returnJson);
+        assert (returnedResponse.equals(genericResponse));
+    }
+
+    @Test
+    public void userSignUpFail() throws Exception {
+        GenericResponse genericResponse = new GenericResponse();
+        genericResponse.setSuccess(false);
+        genericResponse.setMessage("User Sign Up Failed");
+
+        Employee e = new Employee();
+        e.setUsername("test");
+        e.setPassword("password");
+
+        String inputJson = employeeAsJsonString(e);
+
+        when(operations.userSignUp(any())).thenReturn(genericResponse);
+
+        String returnJson = mockMvc.perform(post("/users/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(inputJson)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andReturn().getResponse().getContentAsString();
+
+        GenericResponse returnedResponse = stringToGenericResponse(returnJson);
+        assert (returnedResponse.equals(genericResponse));
+    }
+
+    @Test
+    public void userDelete() throws Exception {
+        GenericResponse genericResponse = new GenericResponse();
+        genericResponse.setSuccess(true);
+        genericResponse.setMessage("Employee Deleted Successfully");
+
+        when(operations.deleteUser(1)).thenReturn(genericResponse);
+
+        String returnJson = mockMvc.perform(delete("/users/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn().getResponse().getContentAsString();
+
+        GenericResponse returnedResponse = stringToGenericResponse(returnJson);
+        assert (returnedResponse.equals(genericResponse));
+    }
+
+    @Test
+    public void userDeleteFailNonUser() throws Exception {
+        GenericResponse genericResponse = new GenericResponse();
+        genericResponse.setSuccess(false);
+        genericResponse.setMessage("Employee ID is not valid");
+
+        when(operations.deleteUser(1)).thenReturn(genericResponse);
+
+        String returnJson = mockMvc.perform(delete("/users/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andReturn().getResponse().getContentAsString();
+
+        GenericResponse returnedResponse = stringToGenericResponse(returnJson);
+        assert (returnedResponse.equals(genericResponse));
+    }
+
+    @Test
+    public void userDeleteFail() throws Exception {
+        GenericResponse genericResponse = new GenericResponse();
+        genericResponse.setSuccess(false);
+        genericResponse.setMessage("Employee Could Not Be Deleted");
+
+        when(operations.deleteUser(1)).thenReturn(genericResponse);
+
+        String returnJson = mockMvc.perform(delete("/users/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andReturn().getResponse().getContentAsString();
+
+        GenericResponse returnedResponse = stringToGenericResponse(returnJson);
+        assert (returnedResponse.equals(genericResponse));
+    }
+
+    public String employeeAsJsonString(Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
