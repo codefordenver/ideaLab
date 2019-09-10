189bfc9cf96b5b085e33a4e2c648c5f16dd65768
Josh Bernstein <joshua.bernstein@outlook.com>
2019-09-09 19:15:18 -0600

    Removed comments in updatePrintJob

--- Backend/src/main/java/idealab/api/operations/PrintJobOperations.java
+++ Backend/src/main/java/idealab/api/operations/PrintJobOperations.java
@@ -195,47 +195,6 @@ public class PrintJobOperations {
         return response;
     }
 
-    public GenericResponse updatePrintJob(PrintJobUpdateRequest dto) {
-        GenericResponse response = new GenericResponse();
-
-        // Prep for when we get data models merged to master
-        // if(dto.isValidStatus())
-        // {
-        // //check if employee id is valid
-        // EmployeeList employeeList =
-        // employeeListRepo.getEmployeeListById(dto.getEmployeeId());
-        //
-        // //check if print id is valid
-        // PrintStatus printStatus =
-        // printStatusRepo.getPrintStatusById(dto.getPrintStatusId());
-        //
-        // if(employeeList != null && printStatus != null) {
-        // //Update print status
-        //
-        //
-        // //return success message
-        // response.setSuccess(true);
-        // response.setMessage("Print Job Updated");
-        // }
-        //
-        // }
-        //
-        // return response;
-
-        if (dto.isValidStatus()) {
-            // check if employee id is valid
-            // check if print id is valid
-            // do any other logic to determine if the update is valid
-            response.setSuccess(true);
-            response.setMessage("Print Job Updated");
-        } else {
-            response.setSuccess(false);
-            response.setMessage("Invalid Status");
-        }
-        return response;
-    }
-
-
     public GenericResponse updatePrintJobStatus(Integer printId, PrintJobUpdateRequest dto) {
         GenericResponse response = new GenericResponse();
         response.setSuccess(false);
