d76dd9c39e498e242daeae16b9a29bcaf249a598
david <david@divlogic.net>
2019-09-14 19:19:07 -0600

    add upload component

--- src/components/Upload/UploadContainer.jsx
+++ src/components/Upload/UploadContainer.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from 'react'
+import Upload from './components/Upload'
 import './UploadContainer.css';
 
-function UploadContainer(){
+function UploadContainer() {
   return (
     <div className={"uploadContainer"}>
       <form>
+        <Upload className={"upload"}>
+        </Upload>
+
         <input
           type='text'
           id='name'
-          placeholder='Name'/>
+          placeholder='Name' />
         <input
           type='text'
           id='color'
-          placeholder='Color'/>
+          placeholder='Color' />
         <input
           id='comments'
-          placeholder='Comments'/>
+          placeholder='Comments' />
         <button className={"shapedButton"} type='submit'>SUMBIT</button>
       </form>
     </div>
