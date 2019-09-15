d2d088d99b3a755aedf48a789bc36bbe614e9731
david <david@divlogic.net>
2019-09-15 00:42:41 -0600

    refactor validationErrorGetter

--- src/components/Upload/UploadContainer.jsx
+++ src/components/Upload/UploadContainer.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react'
-import Upload from './components/Upload'
 import RequestService from '../../util/RequestService';
+import Upload from './components/Upload'
+import BasicInput from '../BasicInput';
 import './UploadContainer.css';
 
 function UploadContainer() {
   const [file, setFile] = useState();
-  const [email, setEmail] = useState();
-  const [customerFirstName, setCustomerFirstName] = useState();
-  const [customerLastName, setCustomerLastName] = useState();
-  const [color, setColor] = useState();
-  const [comments, setComments] = useState();
+  const [email, setEmail] = useState('');
+  const [customerFirstName, setCustomerFirstName] = useState('');
+  const [customerLastName, setCustomerLastName] = useState('');
+  const [color, setColor] = useState('');
+  const [comments, setComments] = useState('');
+
+  const [errors, setErrors] = useState({});
+
+  function onFailure(error) {
+    const validationErrors = RequestService.validationErrorGetter(error);
+    console.log(validationErrors);
+    setErrors(validationErrors);
+  };
+
 
   return (
     <div className={"uploadContainer"}>
+      <span>{errors.form ? errors.form : null}</span>
       <form onSubmit={e => {
         e.preventDefault();
         const formData = new FormData();
@@ -22,34 +33,16 @@ function UploadContainer() {
         formData.append('customerLastName', customerLastName);
         formData.append('color', color);
         formData.append('comments', comments);
-        RequestService.newPrintJob(formData, (response) => console.log(response), (err) => console.log(err));
+        RequestService.newPrintJob(formData, (response) => response, onFailure);
       }}>
         <Upload className={"upload"} callback={(files => setFile(files[0]))}>
         </Upload>
-        <input
-          onChange={e => setCustomerFirstName(e.target.value)}
-          type='text'
-          id='firstName'
-          placeholder='First Name' />
-        <input
-          onChange={e => setCustomerLastName(e.target.value)}
-          type='text'
-          id='lastName'
-          placeholder='Last Name' />
-        <input
-          onChange={e => setEmail(e.target.value)}
-          type='text'
-          id='email'
-          placeholder='Email' />
-        <input
-          onChange={e => setColor(e.target.value)}
-          type='text'
-          id='color'
-          placeholder='Color' />
-        <input
-          onChange={e => setComments(e.target.value)}
-          id='comments'
-          placeholder='Comments' />
+        <p>{errors.file ? errors.file : null}</p>
+        <BasicInput className={"upload"} placeHolder={"First Name"} changeHandler={setCustomerFirstName} error={errors.customerFirstName} />
+        <BasicInput className={"upload"} placeHolder={"Last Name"} changeHandler={setCustomerLastName} error={errors.customerLastName} />
+        <BasicInput className={"upload"} placeHolder={"Email"} changeHandler={setEmail} error={errors.email} />
+        <BasicInput className={"upload"} placeHolder={"Color"} changeHandler={setColor} error={errors.color} />
+        <BasicInput className={"upload"} placeHolder={"Comments"} changeHandler={setComments} error={errors.comments} />
         <button className={"shapedButton"} type='submit'>SUMBIT</button>
       </form>
     </div>
--- src/util/RequestService.js
+++ src/util/RequestService.js
@@ -5,8 +5,17 @@ const baseurl = 'http://localhost:8080';
 const RequestService = {
 
     validationErrorGetter: function (axiosError) {
+        if (axiosError.response.status > 499) {
+            return { form: 'There was a problem with what you were trying to do.' };
+        }
+
         const errors = axiosError.response.data.errors
-        return errors;
+        const newErrorState = {};
+        errors.forEach(error => {
+            console.log(error);
+            newErrorState[error.field] = error.defaultMessage;
+        });
+        return newErrorState;
     },
 
     signUp: function (payload, thenCallback, catchCallback) {
@@ -15,7 +24,7 @@ const RequestService = {
             .catch(catchCallback);
     },
 
-    newPrintJob(payload, thenCallback, catchCallback){
+    newPrintJob(payload, thenCallback, catchCallback) {
         console.log(payload);
         axios.post(baseurl + '/api/printjobs', payload)
             .then(thenCallback)
