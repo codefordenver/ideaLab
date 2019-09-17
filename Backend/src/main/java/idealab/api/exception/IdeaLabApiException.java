93c5d8515be77e49f03dc6433614a231f1b0029c
Hannah Amundson <hannahkamundson@gmail.com>
2019-09-16 20:42:15 -0600

    Added serial version uid

--- Backend/src/main/java/idealab/api/exception/IdeaLabApiException.java
+++ Backend/src/main/java/idealab/api/exception/IdeaLabApiException.java
@@ -1,6 +1,8 @@
 package idealab.api.exception;
 
 public class IdeaLabApiException extends RuntimeException{
+
+    private static final long serialVersionUID = -5477121430747808511L;
     private ErrorType errorType;
 
     public IdeaLabApiException(ErrorType errorType){
