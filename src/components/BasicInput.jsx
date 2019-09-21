495a49fd65bd272450f6c47e4182183d02fc3fa5
david <david@divlogic.net>
2019-09-20 19:58:48 -0600

    fix BasicInput.jsx not being committed

---
+++ src/components/BasicInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BasicInput = (props) => {
+
+    return (
+        <div>
+            <input
+                onChange={e => props.changeHandler(e.target.value)}
+                type='text'
+                placeholder={props.placeHolder} />
+            <span>{props.error ? props.error : null}</span>
+        </div>
+    );
+}
+
+export default BasicInput;
