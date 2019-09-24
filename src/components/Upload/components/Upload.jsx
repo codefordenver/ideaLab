5fa5d1297f9adb92e6e5d2748e367cb51cf9c878
david <david@divlogic.net>
2019-09-23 20:52:22 -0600

    add filesize validation to front end

--- src/components/Upload/components/Upload.jsx
+++ src/components/Upload/components/Upload.jsx
@@ -1,22 +1,29 @@
-import React, { useCallback } from 'react'
-import { useDropzone } from 'react-dropzone'
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 function Upload(props) {
-    const onDrop = useCallback(acceptedFiles => {
-        props.callback(acceptedFiles);
-    }, [])
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+  const [fileSizeError, setFileSizeError] = useState(false);
+  const onDrop = useCallback(acceptedFiles => {
+    if (acceptedFiles.length > 0) {
+      props.callback(acceptedFiles);
+    } else {
+      setFileSizeError(true);
+    }
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: onDrop,
+    maxSize: 100000000,
+    multiple: false,
+  });
 
-    return (
-        <div className={"upload"} {...getRootProps()}>
-            <input {...getInputProps()} />
-            {
-                isDragActive ?
-                    <p>upload or drag STL files</p> :
-                    <p>upload STL files</p>
-            }
-        </div>
-    )
+  return (
+    <div className={'upload'} {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? <p>upload or drag STL files</p> : <p>upload STL files</p>}
+      <span>Max file size is 100MB</span>
+      {fileSizeError ? <span className={"error"}>Max file size exceeded</span> : false}
+    </div>
+  );
 }
 
 export default Upload;
