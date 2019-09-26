2e3d2a476a7268922fb40e500b36472677d948e7
Joshua Bernstein <joshua.bernstein@outlook.com>
2019-09-26 16:58:49 -0600

    Updated the nav to hide all the inactive links until someone is logged in. (#172)

--- src/SidebarNavigation.jsx
+++ src/SidebarNavigation.jsx
@@ -5,32 +5,39 @@ import { NavLink } from 'react-router-dom';
 
 class SidebarNavigation extends React.Component {
   render() {
+    const authenticatedNav = (
+      <>
+        <li>
+          <NavLink to="/upload" activeClassName={'selected'}>
+            upload
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/queue" activeClassName={'selected'}>
+            queue
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/manageaccounts" activeClassName={'selected'}>
+            manage accounts
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/analytics" activeClassName={'selected'}>
+            analytics
+          </NavLink>
+        </li>
+      </>
+    );
+
     return (
       <AuthContext.Consumer>
         {context => {
           return (
             <nav className={'grid-item-nav'}>
               <ul className={'navList'}>
-                <li>
-                  <NavLink to="/upload" activeClassName={'selected'}>
-                    upload
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/queue" activeClassName={'selected'}>
-                    queue
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/manageaccounts" activeClassName={'selected'}>
-                    manage accounts
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/analytics" activeClassName={'selected'}>
-                    analytics
-                  </NavLink>
-                </li>
+                {context.authenticated && authenticatedNav}
+
                 <li onClick={context.authenticated ? this.props.logout : null}>
                   <NavLink to="/login" activeClassName={'selected'}>
                     {context.authenticated ? 'log out' : 'log in'}
