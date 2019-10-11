1a847747f781556871cfe5d8cea314c190973647
divlogic <54046208+divlogic@users.noreply.github.com>
2019-10-10 20:27:21 -0600

    add loading indicator to Queue page (#208)

---
+++ Frontend/src/components/globalStyles/Loader.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import styles from './Loader.css';
+
+const Loader = ({ selectedTab, tabOptions, setView }) => {
+  return (
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
