0ba36e8b800b5923deede2d9b78094604a4a23af
Kyle Middleton <kylemid321@gmail.com>
2019-11-14 20:35:58 -0700

    import and add CirclePicker component from react-colors

--- Frontend/src/components/AdminSettings/AdminSettings.jsx
+++ Frontend/src/components/AdminSettings/AdminSettings.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
+import { CirclePicker } from 'react-color';
 
 const AdminSettings = () => {
-  return <div>SETTINGS GO HERE</div>;
+  return (
+    <div>
+      <h2>SETTINGS GO HERE</h2>
+      <CirclePicker />
+    </div>
+  );
 };
 
 export default AdminSettings;
