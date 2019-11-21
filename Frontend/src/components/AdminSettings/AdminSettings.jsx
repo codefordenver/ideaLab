51b7c6d9f222a3478da1fa82154f8f054aea611c
Kyle Middleton <kylemid321@gmail.com>
2019-11-21 14:13:55 -0700

    setup placeholder text and containers

--- Frontend/src/components/AdminSettings/AdminSettings.jsx
+++ Frontend/src/components/AdminSettings/AdminSettings.jsx
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
 import { CirclePicker } from 'react-color';
+import './AdminSettings.css';
 
 const AdminSettings = () => {
   return (
-    <div>
-      <CirclePicker />
+    <div className="adminSettingsContainer">
+      <body>
+        <div className="headerContainer">Header Goes Here</div>
+        <div className="colorPickerContainer">
+          <CirclePicker />
+        </div>
+        <div className="sectionContainer">
+          <div className="adminSettingsSectionHeader">Sample List of Things</div>
+          <hr className="horizontalLine" />
+          <div className="listContainer">
+            <li className="sectionListItem">Item 1</li>
+            <li className="sectionListItem">Item 2</li>
+            <li className="sectionListItem">Item 3</li>
+          </div>
+        </div>
+        <input className="adminSettingsInput" placeholder="Sample input" label="Input" />
+        <div className="adminSettingsButton">Button</div>
+
+      </body>
     </div>
   );
 };
