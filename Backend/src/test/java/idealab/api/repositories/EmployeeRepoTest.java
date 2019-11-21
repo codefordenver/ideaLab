5d27594f0ac34c5fccdafeb2c63b41af717950b5
Hannah Amundson <amundson@opttek.com>
2019-11-20 21:30:44 -0700

    Fixed the test I made

--- Backend/src/test/java/idealab/api/repositories/EmployeeRepoTest.java
+++ Backend/src/test/java/idealab/api/repositories/EmployeeRepoTest.java
@@ -1,7 +1,11 @@
+package idealab.api.repositories;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import idealab.api.repositories.EmployeeRepo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class EmployeeRepoTest {
