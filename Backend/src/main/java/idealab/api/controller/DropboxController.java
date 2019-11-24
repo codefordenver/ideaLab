58753518151c5deb26415d9871c8cb3ff1f5cc51
will-estes <will_estes@yahoo.com>
2019-11-24 11:38:12 -0700

    working example of properties being updated

--- Backend/src/main/java/idealab/api/controller/DropboxController.java
+++ Backend/src/main/java/idealab/api/controller/DropboxController.java
@@ -1,28 +1,36 @@
 package idealab.api.controller;
 
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import idealab.api.dto.request.UpdateFilePathRequest;
 import idealab.api.dto.response.DataResponse;
+import idealab.api.dto.response.GenericResponse;
 import idealab.api.model.PrintJob;
+import idealab.api.operations.PropertyOperations;
 import idealab.api.service.FileService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/dropbox")
 public class DropboxController {
 
     private final FileService fileService;
+    private final PropertyOperations propertyOperations;
 
-    public DropboxController(FileService fileService) {
+    public DropboxController(FileService fileService, PropertyOperations propertyOperations) {
         this.fileService = fileService;
+        this.propertyOperations = propertyOperations;
     }
 
     @PutMapping("/file-path")
     public DataResponse<PrintJob> updateFilePath(@RequestBody UpdateFilePathRequest request) {
         return fileService.updateFilePath(request);
+    }
+
+    @PostMapping("/token")
+    public GenericResponse updateToken(@RequestBody Map<String, String> mapParam) {
+        String token = mapParam.get("token");
+        return propertyOperations.updateDropboxToken(token);
     }
 
 }
--- Backend/src/main/java/idealab/api/operations/PropertyOperations.java
+++ Backend/src/main/java/idealab/api/operations/PropertyOperations.java
@@ -1,25 +1,61 @@
 package idealab.api.operations;
 
-import org.apache.commons.configuration2.Configuration;
-import org.apache.commons.configuration2.FileBasedConfiguration;
-import org.apache.commons.configuration2.PropertiesConfiguration;
-import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
-import org.apache.commons.configuration2.builder.fluent.Parameters;
+import idealab.api.dto.response.GenericResponse;
+import idealab.api.exception.ErrorType;
+import idealab.api.exception.IdeaLabApiException;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import javax.el.PropertyNotFoundException;
+
+import static idealab.api.exception.ErrorType.VALIDATION_ERROR;
 
 
 @Service
 public class PropertyOperations {
 
     public static final String DROPBOX_FILENAME = "dropbox.properties";
+    private static final String PROPERTY_NAME = "dropbox.ACCESS_TOKEN";
 
-    public void updateDropboxToken(String token) throws Exception {
-        Parameters params = new Parameters();
-        FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
-                new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
-                        .configure(params.properties().setFileName(DROPBOX_FILENAME));
-        Configuration config = builder.getConfiguration();
-        config.setProperty("dropbox.ACCESS_TOKEN", token);
-        builder.save();
+    public String dropboxFilePath;
+
+    @Autowired
+    public PropertyOperations(@Value("${dropbox.ABSOLUTE_FILE_PATH}") String dropboxFilePath) {
+        this.dropboxFilePath = dropboxFilePath;
+    }
+
+    public GenericResponse updateDropboxToken(String token) {
+        return updateToken(token, PROPERTY_NAME, dropboxFilePath, DROPBOX_FILENAME);
+    }
+
+    // NOTE: This test will NOT actually save over the test properties file. That's because it requires an
+    // absolute path which will be different per machine. However, this does run through the logic so
+    // it's important to keep this.
+    public GenericResponse testUpdateDropboxToken(String token, String propertyName, String filePath) {
+        return updateToken(token, propertyName, filePath, "");
+    }
+
+    private GenericResponse updateToken(String token, String propertyName, String filePath, String fileName) {
+        if(token == null || token.isEmpty())
+            throw new IdeaLabApiException(VALIDATION_ERROR, "Token is null or blank");
+
+        if(dropboxFilePath == null)
+            throw new PropertyNotFoundException("Dropbox file path is null");
+
+        PropertiesConfiguration config = null;
+        try {
+            config = new PropertiesConfiguration(filePath + fileName);
+            config.setProperty(propertyName, token);
+            config.save();
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+            throw new IdeaLabApiException(ErrorType.GENERAL_ERROR, "Dropbox token not updated");
+        }
+
+        return new GenericResponse(true, "Token updated successfully", HttpStatus.ACCEPTED);
     }
 }
