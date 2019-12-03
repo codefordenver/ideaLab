16214d16a6f2df030f8f329996f0bf2e0096b2ce
will-estes <will_estes@yahoo.com>
2019-12-02 20:17:31 -0700

    initial

---
+++ Backend/src/main/java/idealab/api/controller/MailController.java
@@ -0,0 +1,40 @@
+package idealab.api.controller;
+
+import idealab.api.dto.request.MailRequest;
+import idealab.api.dto.response.GenericResponse;
+import idealab.api.service.EmailService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/*
+*   THIS IS A TEST API ENDPOINT. THIS IS NOT TO GO OUT TO THE END USER/CUSTOMER
+*   TODO: DELETE THIS NONSENSE WHEN WE GO TO PRODUCTION
+* */
+
+
+@RestController
+@RequestMapping("/api/mail")
+public class MailController {
+
+    private EmailService emailService;
+
+    public MailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> sendEmail(@RequestBody MailRequest mailRequest) {
+        emailService.sendSimpleMessage(mailRequest);
+        GenericResponse response = new GenericResponse();
+        response.setSuccess(true);
+        response.setMessage("Email sent successfully");
+        response.setHttpStatus(HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(response, response.getHttpStatus());
+    }
+
+}
