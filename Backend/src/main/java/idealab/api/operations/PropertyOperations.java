80ac771bb2eaa9fd9c8a1556ffddb83c90f84635
amundson <amundson@opttek.com>
2019-12-26 13:01:17 -0700

    Updated the dropbox property to be included in the application properties file

--- Backend/src/main/java/idealab/api/operations/PropertyOperations.java
+++ Backend/src/main/java/idealab/api/operations/PropertyOperations.java
@@ -1,60 +1,72 @@
 package idealab.api.operations;
 
-import idealab.api.dto.response.GenericResponse;
-import idealab.api.exception.ErrorType;
-import idealab.api.exception.IdeaLabApiException;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
+import static idealab.api.exception.ErrorType.VALIDATION_ERROR;
+
+import java.io.File;
 
 import javax.el.PropertyNotFoundException;
 
-import static idealab.api.exception.ErrorType.VALIDATION_ERROR;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import idealab.api.dto.response.GenericResponse;
+import idealab.api.exception.ErrorType;
+import idealab.api.exception.IdeaLabApiException;
 
 
 @Service
 public class PropertyOperations {
+    
+    private static final String DROPBOX_PROPERTY_NAME = "dropbox.ACCESS_TOKEN";
 
-    public static final String DROPBOX_FILENAME = "dropbox.properties";
-    private static final String PROPERTY_NAME = "dropbox.ACCESS_TOKEN";
+    private File propertiesFilePath = new File(getClass().getClassLoader().getResource("application.properties").getFile());
 
-    public String dropboxFilePath;
-
-    @Autowired
-    public PropertyOperations(@Value("${dropbox.ABSOLUTE_FILE_PATH}") String dropboxFilePath) {
-        this.dropboxFilePath = dropboxFilePath;
+    public PropertyOperations() {
     }
 
+    /**
+     * This updates the dropbox token.
+     * @param token
+     * @return
+     */
     public GenericResponse updateDropboxToken(String token) {
-        return updateToken(token, PROPERTY_NAME, dropboxFilePath, DROPBOX_FILENAME);
+        String errorMessage = "Dropbox token not updated: ";
+        boolean didItWork = updateToken(token, DROPBOX_PROPERTY_NAME, errorMessage);
+
+        // This will always return true or else it will throw an exception
+        return new GenericResponse(didItWork, "Token updated successfully", HttpStatus.ACCEPTED);
     }
 
     // NOTE: This test will NOT actually save over the test properties file. That's because it requires an
     // absolute path which will be different per machine. However, this does run through the logic so
     // it's important to keep this.
-    public GenericResponse testUpdateDropboxToken(String token, String propertyName, String filePath) {
-        return updateToken(token, propertyName, filePath, "");
+    public GenericResponse testUpdateDropboxToken(String token, String propertyName) {
+        boolean didItWork = updateToken(token, propertyName, "Fake error");
+        if (didItWork) {
+            return new GenericResponse(true, "Token updated successfully", HttpStatus.ACCEPTED);
+        } else {
+            return new GenericResponse();
+        }
     }
 
-    private GenericResponse updateToken(String token, String propertyName, String filePath, String fileName) {
-        if(token == null || token.isEmpty())
+    private boolean updateToken(String newPropertyValue, String propertyName, String errorMessage) {
+        if(newPropertyValue == null || newPropertyValue.isEmpty())
             throw new IdeaLabApiException(VALIDATION_ERROR, "Token is null or blank");
 
-        if(dropboxFilePath == null)
-            throw new PropertyNotFoundException("Dropbox file path is null");
+        if(propertiesFilePath == null)
+            throw new PropertyNotFoundException("Application properties file path is null");
 
         PropertiesConfiguration config = null;
         try {
-            config = new PropertiesConfiguration(filePath + fileName);
-            config.setProperty(propertyName, token);
+            config = new PropertiesConfiguration(propertiesFilePath);
+            config.setProperty(propertyName, newPropertyValue);
             config.save();
         } catch (ConfigurationException e) {
-            throw new IdeaLabApiException(ErrorType.GENERAL_ERROR, "Dropbox token not updated: " + e.getMessage());
+            throw new IdeaLabApiException(ErrorType.GENERAL_ERROR, errorMessage + e.getMessage());
         }
 
-        return new GenericResponse(true, "Token updated successfully", HttpStatus.ACCEPTED);
+        return true;
     }
 }
