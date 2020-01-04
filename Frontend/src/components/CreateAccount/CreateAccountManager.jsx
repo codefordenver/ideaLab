936061332c63e818244df06873bf823e54314e69
Emilie Thoreson <emilie.thoreson@bombbomb.com>
2020-01-04 15:40:32 -0700

    hook up green success message

--- Frontend/src/components/CreateAccount/CreateAccountManager.jsx
+++ Frontend/src/components/CreateAccount/CreateAccountManager.jsx
@@ -13,9 +13,11 @@ const CreateAccountManager = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errors, setErrors] = useState({});
+  const [showingSuccess, showSuccess] = useState(false);
 
-  function onSuccess(response) {
-    console.log(response.data.message);
+  function onSuccess() {
+    showSuccess(true);
+    setTimeout(() => showSuccess(false), 3000);
   }
 
   function onFailure(error) {
@@ -33,35 +35,33 @@ const CreateAccountManager = () => {
       role: role,
       username: username,
     };
-
     RequestService.signUp(payload, onSuccess, onFailure);
   };
 
-  const renderErrors = () => {
-    return null;
-  };
-
   const renderSuccessMessage = () => {
-    return null;
+    const message = showingSuccess ? (
+      <h3 className="successCreateAccount">Successfully Created Account!</h3>
+    ) : null;
+    return message;
   };
 
-  // function renderErrors() {
-  //   const errorMessages = [
-  //     'role',
-  //     'firstName',
-  //     'lastName',
-  //     'username',
-  //     'email',
-  //     'password',
-  //   ].map(field => {
-  //     if (errors[field]) {
-  //       return <p>{field + ' ' + errors[field]}</p>;
-  //     } else {
-  //       return null;
-  //     }
-  //   });
-  //   return errorMessages;
-  // }
+  function renderErrors() {
+    const errorMessages = [
+      'role',
+      'firstName',
+      'lastName',
+      'username',
+      'email',
+      'password',
+    ].map(field => {
+      if (errors[field]) {
+        return <p>{field + ' ' + errors[field]}</p>;
+      } else {
+        return null;
+      }
+    });
+    return errorMessages;
+  }
 
   return (
     <div className="create-account-container">
@@ -83,37 +83,37 @@ const CreateAccountManager = () => {
             placeHolder="email"
             value={email}
             type="email"
-            changeHandler={e => setEmail(e.target.value)}
+            changeHandler={e => setEmail(e)}
           />
           <BasicInput
             name="username"
             placeHolder="username"
             value={username}
             type="text"
-            changeHandler={e => setUsername(e.target.value)}
+            changeHandler={e => setUsername(e)}
           />
           <BasicInput
             name="password"
             placeHolder="password"
             value={password}
             type="text"
-            changeHandler={e => setPassword(e.target.value)}
+            changeHandler={e => setPassword(e)}
           />
           <BasicInput
             name="firstName"
-            placeHolder="First Name"
+            placeHolder="first name"
             value={firstName}
             type="text"
-            changeHandler={e => setFirstName(e.target.value)}
+            changeHandler={e => setFirstName(e)}
           />
           <BasicInput
             name="lastName"
-            placeHolder="Last Name"
+            placeHolder="last name"
             value={lastName}
             type="text"
-            changeHandler={e => setLastName(e.target.value)}
+            changeHandler={e => setLastName(e)}
           />
-          <div>
+          <div className="roleRadioButtons">
             <label>
               <input
                 name="role"
@@ -122,7 +122,7 @@ const CreateAccountManager = () => {
                 value={'STAFF'}
                 onChange={e => setRole(e.target.value)}
               />
-              Staff
+              <span>STAFF</span>
             </label>
             <label>
               <input
@@ -132,11 +132,11 @@ const CreateAccountManager = () => {
                 value={'ADMIN'}
                 onChange={e => setRole(e.target.value)}
               />
-              Admin
+              <span>ADMIN</span>
             </label>
           </div>
           <button className="createNewAccountButton" type="submit">
-            Create Account
+            CREATE ACCOUNT
           </button>
         </form>
       </div>
