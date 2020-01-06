0fcbdd92c390de1adcb16c7ae535e02fcc479e8a
Luke Mueller <muelll79@hotmail.com>
2020-01-05 17:35:30 -0700

    adds first button styles

--- Frontend/src/components/Queue/components/SearchBar.jsx
+++ Frontend/src/components/Queue/components/SearchBar.jsx
@@ -1,24 +1,51 @@
 import React, { useState } from 'react';
 
-const SearchBar = ({ filterByTerm }) => {
+import './SearchBar.css';
+
+const SearchBar = props => {
   const [search, setSearch] = useState('');
+  const [searched, setSearched] = useState(false);
 
   const searchQuery = event => {
     setSearch(event.target.value);
-    filterByTerm(event.target.value);
   };
 
+  const submit = event => {
+    props.submit(event);
+    setSearch('');
+    setSearched(true);
+    event.preventDefault();
+  };
+
+  const goBack = () => {
+    setSearched(false);
+    props.fetchQueueData();
+  };
+
+  let submitBtn, goBackBtn;
+  if (!searched) {
+    submitBtn = <button type="submit" />;
+  } else if (searched) {
+    goBackBtn = (
+      <button style={{ background: 'red' }} onClick={goBack}>
+        Go Back
+      </button>
+    );
+  }
+
   return (
-    <div className="search-container">
+    <form className="search-container" onSubmit={submit}>
       <input
         className="search-bar"
+        name="input"
         value={search}
         onChange={searchQuery}
         type="text"
         placeholder="Search"
       />
-      <button type="submit" />
-    </div>
+      {submitBtn}
+      {goBackBtn}
+    </form>
   );
 };
 
